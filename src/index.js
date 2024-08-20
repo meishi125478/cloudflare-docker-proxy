1b8a160e12d119a746cd141fbe88ff7877b59ace
meishi125478 <29742438+meishi125478@users.noreply.github.com>
2024-08-20 13:24:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.wljsyph.top": "https://registry-1.docker.io",
+  "quay.wljsyph.top": "https://quay.io",
+  "gcr.wljsyph.top": "https://gcr.io",
+  "k8s-gcr.wljsyph.top": "https://k8s.gcr.io",
+  "k8s.wljsyph.top": "https://registry.k8s.io",
+  "ghcr.wljsyph.top": "https://ghcr.io",
+  "cloudsmith.wljsyph.top": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
